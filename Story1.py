1b2a6db5ebad1e20dae2eeb9b9389a22a597b29c
seantobin <sean.tobin@kearsarge.org>
2017-10-12 13:20:28 -0400

    wrote story 1

--- Story1.py
+++ Story1.py
@@ -9,10 +9,18 @@ def story1(debug= False):
 	store1 = getWord("A Store: ", debug)
 	
 	out = ""
-	out += "One day me and my friend, " + friend1
-	out += ", walked " + distance1
-	out += " miles to " + store1
-	out += ". " + friend1
-	out += " loves " + store1
-	
+	out += "There once was a little boy who got in the wrong, " + car1
+	out += ", He wanted some  " + food1
+	out += " Once he got the " + food1
+	out += "he left and decided to eat the " + food1
+	out += " after he ate it he went home and played " + game1
+	out += " as he was playing " + game1
+    out += " his " + person1
+    out += " came up and said " + word1
+    out += " the little boy didnt listen to " + person1
+    out += " so " + person1
+    out += " told him to go to " + place1
+    
+    
+    
 	return out
