4a3f8f0850539aea3805c814a9fa58d4d3d703ac
HackedbyNoah <noah.gray@kearsarge.org>
2017-10-13 13:50:39 -0400

    Fixed errors that stopped from running and added to swear list

--- Story1.py
+++ Story1.py
@@ -2,19 +2,19 @@ from Getter import *
 
 
 def story1(debug= False):
-	if debug: print "--In story1 function--"
-	
-	friend1 = getWord("A Name: ", debug)
-	distance1 = getNumber("A Number: ", debug)
-	store1 = getWord("A Store: ", debug)
-	
-	out = ""
-	out += "There once was a little boy who got in the wrong, " + car1
-	out += ", He wanted some  " + food1
-	out += " Once he got the " + food1
-	out += "he left and decided to eat the " + food1
-	out += " after he ate it he went home and played " + game1
-	out += " as he was playing " + game1
+    if debug: print "--In story1 function--"
+    
+    friend1 = getWord("A Name: ", debug)
+    distance1 = getNumber("A Number: ", debug)
+    store1 = getWord("A Store: ", debug)
+    
+    out = ""
+    out += "There once was a little boy who got in the wrong, " + car1
+    out += ", He wanted some  " + food1
+    out += " Once he got the " + food1
+    out += "he left and decided to eat the " + food1
+    out += " after he ate it he went home and played " + game1
+    out += " as he was playing " + game1
     out += " his " + person1
     out += " came up and said " + word1
     out += " the little boy didnt listen to " + person1
@@ -25,4 +25,4 @@ def story1(debug= False):
     out += " then " + person2
     out += " killed him with a " + weapon1 
     
-	return out
+    return out
--- Story2.py
+++ Story2.py
@@ -2,18 +2,18 @@ from Getter import *
 
 
 def story2(debug= False):
-	if debug: print "--In story1 function--"
-	
-	friend1 = getWord("A Name: ", debug)
-	distance1 = getNumber("A Number: ", debug)
-	store1 = getWord("A Store: ", debug)
-	
-	out = ""
-	out += " One day " + name1
-	out += " was on his way to the  " + place1
-	out += " when he spotted a  " + animal1
-	out += " The " + animal1
-	out += " ran up to " + name1 
+    if debug: print "--In story1 function--"
+    
+    friend1 = getWord("A Name: ", debug)
+    distance1 = getNumber("A Number: ", debug)
+    store1 = getWord("A Store: ", debug)
+    
+    out = ""
+    out += " One day " + name1
+    out += " was on his way to the  " + place1
+    out += " when he spotted a  " + animal1
+    out += " The " + animal1
+    out += " ran up to " + name1 
     out += " and decided to  " + action1
     out += " him/her the" + animal1
     out += " then ran away and  " + name1
@@ -42,4 +42,4 @@ def story2(debug= False):
     
     
     
-	return out
+    return out
