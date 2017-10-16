8cdc45b9ac78d5e6450a31c7d135631b50725310
seantobin <sean.tobin@kearsarge.org>
2017-10-16 13:07:12 -0400

    started story3

--- Story3.py
+++ Story3.py
@@ -1,1 +1,12 @@
+from Getter import *
 
+
+def story3(debug= False):
+	if debug: print "--In story1 function--"
+	
+	friend1 = getWord("A Name: ", debug)
+	distance1 = getNumber("A Number: ", debug)
+	store1 = getWord("A Store: ", debug)
+	
+	out += " 
+	out += 
