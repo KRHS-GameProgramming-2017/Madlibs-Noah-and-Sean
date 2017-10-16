e4e36d8399224b11cf20177ca80076de97f331f8
HackedbyNoah <noah.gray@kearsarge.org>
2017-10-16 13:35:29 -0400

    added and almost finished it

--- Story2.py
+++ Story2.py
@@ -4,24 +4,27 @@ from Getter import *
 def story2(debug= False):
     if debug: print "--In story1 function--"
     
-    friend1 = getWord("A Name: ", debug)
-    distance1 = getNumber("A Number: ", debug)
-    store1 = getWord("A Store: ", debug)
-    
+    name1 = getWord("A Name: ", debug)
+    place1 = getWord("A place: ", debug)
+    animal1 = getWord("An animal: ", debug)
+    action1 = getWord("An action: ", debug)
+    feeling1 = getWord("A feeling: ", debug)
+    action2 = getWord("An action: ", debug)
     out = ""
     out += " One day " + name1
-    out += " was on his way to the  " + place1
+    out += " was on his way to  " + place1
     out += " when he spotted a  " + animal1
     out += " The " + animal1
     out += " ran up to " + name1 
     out += " and decided to  " + action1
-    out += " him/her the" + animal1
+    out += " him/her the  " + animal1
     out += " then ran away and  " + name1
-    out += " became very " + noun1
+    out += " became very " + feeling1
     out += " and began to chase the " + animal1
-    out += " and  " + action3
-    out += " the  " + animal1
-    out += " Died from the  " + action3
+    out += " when his friend  " + name2
+    out += " came to  " + action2
+    out += " the animal when he   " + action2
+    out += " came to  " + action2
     
     
     
