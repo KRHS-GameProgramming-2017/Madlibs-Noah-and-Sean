b4d300e41eb08bcd36c6297699afd3d1218ee2a9
seantobin <sean.tobin@kearsarge.org>
2017-10-16 13:35:55 -0400

    edited story 3 and 1

--- Story3.py
+++ Story3.py
@@ -8,5 +8,5 @@ def story3(debug= False):
 	distance1 = getNumber("A Number: ", debug)
 	store1 = getWord("A Store: ", debug)
 	
-	out += " 
-	out += 
+	out += " Good afternoon said " + name1
+	out += " to Billy, as he was walking towards " + building1
